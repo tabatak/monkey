07be807538cc7f546fe4e9ef9578fbab116a3915
tabatak <hamamoto.kazuto@gmail.com>
2017-06-18 10:33:01 +0900

    add ascii art of MONKEY

--- main.go
+++ main.go
@@ -7,11 +7,21 @@ import (
 	"os/user"
 )
 
+var monkey = `                                                                   
+ _|      _|    _|_|    _|      _|  _|    _|  _|_|_|_|  _|      _|  
+ _|_|  _|_|  _|    _|  _|_|    _|  _|  _|    _|          _|  _|    
+ _|  _|  _|  _|    _|  _|  _|  _|  _|_|      _|_|_|        _|      
+ _|      _|  _|    _|  _|    _|_|  _|  _|    _|            _|      
+ _|      _|    _|_|    _|      _|  _|    _|  _|_|_|_|      _|      
+                                                                   
+`
+
 func main() {
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf(monkey)
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
